a96f9f586e1d0102e67a5a328db85207c719c309
firvain <etsipis@terracognita.gr>
2019-03-26 23:44:49 +0200

    change app name and add router middleware

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+// create middleware to handle the serving the app
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
+// Catch all routes and redirect to the index file
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
 const port = process.env.PORT || 80;
 app.listen(port);
