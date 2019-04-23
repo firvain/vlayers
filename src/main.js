4c044013a6a73e274c2f7f45a53c64e4cc259f0a
firvain <etsipis@terracognita.eu>
2019-04-23 10:49:13 +0300

    axios as plugin and placeholder about

--- src/main.js
+++ src/main.js
@@ -4,14 +4,13 @@ import "./plugins/vuetify";
 import "./plugins/vuelayers";
 import "./plugins/vuex-i18n";
 import "./plugins/vue2-filters.js";
+import AxiosPlugin from "./plugins/axios.js";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/index.js";
-import Axios from "axios";
 
 Vue.config.productionTip = false;
-
-Vue.prototype.$http = Axios;
+Vue.use(AxiosPlugin, "$axios");
 
 new Vue({
   router,
