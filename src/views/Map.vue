46342ee4f0f26c10d7e5fbee2350495af97c7d60
firvain <etsipis@terracognita.eu>
2019-04-24 15:23:26 +0300

    add navigation guard at map view

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -15,12 +15,16 @@ export default {
     return {};
   },
   methods: {
-    ...mapActions("app", ["UPDATE_LOADING"])
+    ...mapActions("app", ["UPDATE_LOADING", "UPDATE_APP_STATUS"])
   },
   beforeRouteEnter(to, from, next) {
     next(vm => {
       vm.UPDATE_LOADING(true);
     });
+  },
+  beforeRouteLeave(to, from, next) {
+    this.UPDATE_APP_STATUS("display");
+    next();
   }
 };
 </script>
